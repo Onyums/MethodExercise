a00eaa0ff96da770b74ec68e24c95f8f7403c3f3
Forrest Boyd <forrestboyd1998@gmail.com>
2020-01-17 10:36:26 -0600

    initial

---
+++ Methodzzzzzzzz/Methodzzzzzzzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methodzzzzzzzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string word = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band = (Console.ReadLine());
+            Console.WriteLine($"{word} is a {color} person who thinks that {band} is a bunch of disguised {animal}s.");
+
+        }
+    }
+}
